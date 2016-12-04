a6436463cf2ff3ece3aaaed4f5b5d790670c2a6f
Jason Clark <mithereal@gmail.com>
2016-12-03 18:12:28 -0700

    release 2.0

--- src/lib/Poison/Poison_Interface.php
+++ src/lib/Poison/Poison_Interface.php
@@ -4,7 +4,7 @@ Title: BotPoison
 Description: Automatically trap, block and poison bots that don't obey robots.txt rules
 Project URL: https://github.com/mithereal/botpoison
 Author: Jason Clark (mithereal@gmail.com)
-Release: 10.13.2016
+Release: 12.03.2016
 Version: 2.0
 */
 namespace Mithereal\Botpoison;
